1d4837f516903eaeaaf31c5a5220ca589b22c143
Sharad Raval <105463211+TheRockNo04@users.noreply.github.com>
2022-10-28 20:42:06 +0530

    Add files via upload

---
+++ #7_Decode Ways.py
@@ -0,0 +1,20 @@
+from random import randint
+
+mapping = dict()
+encoded_message = '111'
+count=0
+string = ""
+
+for i in range(1, 27):
+    mapping[str(i)] = chr(96+i) 
+#print(mapping)
+#print(encoded_message)
+
+for i in range(3):
+    for c in encoded_message:
+        if not count:
+            string = string + mapping[c].value
+        elif mapping[]+mapping:
+
+            if :
+    count += 1
